a3d5ce1698e4f5e1ccde297aa7d8e42a0d7768c0
sporter399 <sjporter399@gmail.com>
2019-06-20 12:49:33 -0600

    another commit to broken

--- main.py
+++ main.py
@@ -7,8 +7,23 @@ app.config['DEBUG'] = True
 form = """
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="style.css">
         
+        <style>
+            form {{
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }}
+            textarea {{
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }}
+
+        </style>
            
     </head>
     <body>
